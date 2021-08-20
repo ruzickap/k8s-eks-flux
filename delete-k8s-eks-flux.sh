b8503243e14a5660d08d5fd7164daf2f81133e57
Petr Ruzicka <petr.ruzicka@gmail.com>
2021-08-20 19:26:46 +0200

    Fix GH Token issue (#10)

--- delete-k8s-eks-flux.sh
+++ delete-k8s-eks-flux.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -n "${GITHUB_TOKEN_K8S_EKS+x}" ]; then
+  export GITHUB_TOKEN="${GITHUB_TOKEN_K8S_EKS}"
+fi
+
 set -euo pipefail
 
 sed -n "/^\`\`\`bash.*/,/^\`\`\`$/p" docs/part-cleanup/README.md | sed "/^\`\`\`*/d" | bash -euxo pipefail
