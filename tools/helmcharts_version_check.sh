24da21bedf95422d67e2049e1c8a40431e1c50f9
Petr Ruzicka <petr.ruzicka@gmail.com>
2021-09-15 22:45:00 +0200

    Improve helmcharts_version_check script

--- tools/helmcharts_version_check.sh
+++ tools/helmcharts_version_check.sh
@@ -11,10 +11,12 @@ while IFS= read -r HELM_LINE ; do
   HELM_REPOSITORY_URL=$( echo "${HELM_LINE}" | cut -f 5 -d " ")
   HELM_CHART_NAME=$( echo "${HELM_LINE}" | cut -f 7 -d " ")
   HELM_CHART_VERSION=$( echo "${HELM_LINE}" | cut -f 9 -d " ")
-  helm repo add --force-update "${HELM_REPOSITORY_NAME}" "${HELM_REPOSITORY_URL}" > /dev/null
+  if [[ ! -f "${HOME}/Library/Caches/helm/repository/${HELM_REPOSITORY_NAME}-charts.txt" ]] ; then
+    helm repo add --force-update "${HELM_REPOSITORY_NAME}" "${HELM_REPOSITORY_URL}" > /dev/null
+  fi
   LATEST_HELM_CHART_VERSION=$(helm search repo "${HELM_REPOSITORY_NAME}/${HELM_CHART_NAME}" --output json | jq -r ".[0].version")
   if [[ "${LATEST_HELM_CHART_VERSION}" != "${HELM_CHART_VERSION}" ]]; then
-    echo "ReleaseName: ${HELM_CHART_NAME} | Current: ${HELM_CHART_VERSION} | Latest version: ${LATEST_HELM_CHART_VERSION}"
+    echo "HelmChart: ${HELM_REPOSITORY_NAME}/${HELM_CHART_NAME} | Current: ${HELM_CHART_VERSION} | Latest version: ${LATEST_HELM_CHART_VERSION}"
   fi
 done < <(grep -R --no-filename -A1 '^kind: HelmRelease' docs/part* | grep ^# | sort)
 ) | column -s \| -t
